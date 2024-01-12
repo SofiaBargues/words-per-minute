70ecbf4be818484e8565eac274d034eb4b708ab8
Sofia Bargues <barguessofia@gmail.com>
2024-01-12 09:13:28 +0000

    algo funciona

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const WORDS = [
+  "ballena",
+  "cabra",
+  "delfin",
+  "elefante",
+  "foca",
+  "gato",
+  "hiena",
+  "iguana",
+  "jirafa",
+  "perro",
+];
+
+export default function WordsPerMinute() {
+  const [word, setWord] = useState(
+    () => WORDS[(Math.random() * WORDS.length) | 0]
+  );
+  const [characterCount, setCharacterCount] = useState(0);
+  const [buffer, setBuffer] = useState("");
+  const [time, setTime] = useState(0);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (buffer === word) {
+      setWord(WORDS[(Math.random() * WORDS.length) | 0]);
+      setCharacterCount((characterCount) => characterCount + word.length);
+    }
+    setBuffer("");
+  }
+
+  useEffect(() => {
+    if (time !== 0) {
+      const timeout = setTimeout(() => setTime(time - 1), 1000);
+      return () => clearTimeout(timeout);
+    }
+  }, [time]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 12,
+        textAlign: "center",
+      }}
+    >
+      {Boolean(time) && <h1 style={{ fontSize: 48 }}>{word} </h1>}
+      <h2> Characters typed: {characterCount}</h2>
+      <h3> Remainig time: {time}</h3>
+      {time !== 0 ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            value={buffer}
+            onChange={(e) => setBuffer(e.target.value)}
+            type="text"
+            autoFocus
+          />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <button
+          onClick={() => {
+            setTime(60);
+            setCharacterCount(0);
+          }}
+        >
+          Play
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      )}
+    </div>
   );
 }
-
-export default App;
